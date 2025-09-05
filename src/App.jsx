8b6e03bda4d9bf2af7ffeff49bbd260fab7bc911
Lars Kydland <larsky@uia.no>
2025-09-05 23:14:12 +0200

    Initial commit with Budsjett-app code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header'
+import Body from './components/Body/Body'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+return (
+    <div>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Sparsom</h1>
+      <p>Student budsjett</p>
+    </header>
+  )
+}
+
+export default Header
